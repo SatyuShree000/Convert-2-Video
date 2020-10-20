13491c1e7e1ea15e904289e3880b46c9e474e240
Satyu <70142319+SatyuShree000@users.noreply.github.com>
2020-10-21 01:31:29 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,41 +1,37 @@
 class Translation(object):
     START_TEXT = """Hello,
-This is a Telegram File convert Bot!
-You can convert any Telegram Document, using this bot! With custom thumbnail
-     
-    """
+I am Telegram File to Streamable File ! Created by @shreevish
+Please send me any Telegram File , And Reply with /converttovideo
+ 🚨 . . . Note : its support almost all Videos except P-Disk Videos & some No Time Showing Videos . . . 🚨
+ 
+🚨 PRON video🔞 Links gives you PERMANENT BAN 🚨
+       ┈┈┈••💙✿❤️✿💚••┈┈┈
+       
+TG File to Streamable Video Converter bot created by @shreevish"""
     ABS_TEXT = " Please don't be selfish."
     BANNED_USER_TEXT = "you are banned"
-    UPGRADE_TEXT = "No upgrade plan 🥰"
-    DOWNLOAD_START = "trying to download"
-    UPLOAD_START = "trying to upload"
+    UPGRADE_TEXT = "@All_Movie_Rockers"
+    DOWNLOAD_START = "Downloading to my server 📥 Please wait...⏳ 🙇🙇🙇 it takes time depend on File Size"
+    UPLOAD_START = "Yay,File Download Successfully 😊 Now Uploading to Telegram 📤"
     RCHD_BOT_API_LIMIT = "size greater than maximum allowed size (50MB). Neverthless, trying to upload."
-    AFTER_SUCCESSFUL_UPLOAD_MSG = "https://t.me/fileConvertRobot"
-    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds. \nPlease rate me if you find me useful. https://t.me/tlgrmcbot?start=anydl_bot-bot \nUploaded in {} seconds."
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "https://t.me/All_Movie_Rockers"
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds. \nPlease Please Join if you find me useful. https://t.me/All_Movie_Rockers \nUploaded in {} seconds."
     NOT_AUTH_USER_TEXT = "Please /upgrade your subscription."
-    SAVED_CUSTOM_THUMB_NAIL = "Custom video / file thumbnail saved. This image will be used in next 24 hr"
-    DEL_ETED_CUSTOM_THUMB_NAIL = "✅ Custom thumbnail cleared succesfully."
-    FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "✅ Media cleared succesfully."
+    SAVED_CUSTOM_THUMB_NAIL = "✅ Custom video / file thumbnail saved. This image will be used in next 24 hr😊"
+    DEL_ETED_CUSTOM_THUMB_NAIL = "✅ Custom thumbnail cleared succesfully.😊"
+    FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "✅ Media cleared succesfully.😊"
     SAVED_RECVD_DOC_FILE = "Document Downloaded Successfully."
     CUSTOM_CAPTION_UL_FILE = ""
-    NO_CUSTOM_THUMB_NAIL_FOUND = "No Custom ThumbNail found."
-    CURENT_PLAN_DETAILS = """Current plan details
---------
-Telegram ID: <code>{}</code>
-Plan name: <a href='https://t.me/InFoTelGroup/174'>{}</a>
-Expires on: {}"""
-    HELP_USER = """I can do:
-👉 <a href="https://telegram.dog/FileConvertRobot">Convert To Streamable video any telegram file</a>
---------
+    NO_CUSTOM_THUMB_NAIL_FOUND = "No Custom ThumbNail found.😞😞"
+    CURENT_PLAN_DETAILS = """
+○ **My Name** : TG File To Streamable Video Converter
+○ **Creator** : <a href="https://telegram.dog/shreevish">꧁★HACKER★꧂</a> 
+○ **Credits** : ```Everyone in this journey```
+○ **Language** : Python3
+○ **Youtube Library** : <a href="https://www.youtube.com/channel/UC83ZzBwbXsxpeRr4wMlulBA">My Channel</a> 
+○ **Source Code** : <a href=" https://telegram.dog/All_Movie_Rockers">👉 Click Here</a> 
+○ **Server** : <a href="https://telegram.dog/All_Movie_Rockers">AMR</a> 
+○ **Build Status** : v1.0[Beta]""" 
 
-🔥 How to deploy : <a href='https://youtu.be/zQamSjXBpJU'>Click here</a>
-
-    © Group   : @InfotelGroup
-    © Channel : @DX_BotZ 
-    © credits : @Spechide
-
-Send /me to know current plan details
-
-"""
 
     REPLY_TO_DOC_FOR_C2V = "Reply to a Telegram media to convert"
